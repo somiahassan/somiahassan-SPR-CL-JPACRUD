7bbeaea1e0c942df38b0834ab6c1eb000e9b535e
Somia Farag <somiafarag5@gmail.com>
2024-08-13 07:01:48 +0000

    lab completed

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class StoreService {
     StoreRepository storeRepository;
     @Autowired
@@ -20,14 +21,14 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -36,13 +37,14 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
-        return null;
-    }
+    Optional<Store> optionalStore = storeRepository.findById(id);
+    return optionalStore.orElse(null);
+}
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        storeRepository.deleteById(id);
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -50,7 +52,14 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        if (optionalStore.isPresent()) {
+            Store store = optionalStore.get();
+            store.setName(replacement.getName());
+            store.setAddress(replacement.getAddress());
+            return storeRepository.save(store);
+        }
         return null;
+    
     }
-
 }
